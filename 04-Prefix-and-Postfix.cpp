82fb9315e29ac48e824fb60bfeb86de864ade2c5
baravebhumika17-spec <baravebhumika17@gmail.com>
2026-09-22 17:35:40 +0530

    Create 04-Prefix-and-Postfix.cpp

---
+++ 04-Prefix-and-Postfix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+private:
+    int value;
+
+public:
+    explicit Counter(int initialValue = 0)
+        : value(initialValue) {}
+
+    // Prefix
+    Counter& operator++()
+    {
+        ++value;
+        return *this;
+    }
+
+    // Postfix
+    Counter operator++(int)
+    {
+        Counter old = *this;
+        ++value;
+        return old;
+    }
+
+    void display() const
+    {
+        cout << value << '\n';
+    }
+};
+
+int main()
+{
+    Counter counter(8);
+
+    cout << "After prefix increment: ";
+    ++counter;
+    counter.display();
+
+    cout << "Value returned by postfix increment: ";
+    Counter oldValue = counter++;
+    oldValue.display();
+
+    cout << "Counter after postfix increment: ";
+    counter.display();
+
+    return 0;
+}
