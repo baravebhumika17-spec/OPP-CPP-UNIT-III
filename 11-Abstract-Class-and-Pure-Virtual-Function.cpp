75b08d0d33eedbd555e67a70f6bb4c6ad6c980e4
baravebhumika17-spec <baravebhumika17@gmail.com>
2026-09-22 17:47:05 +0530

    Create 11-Abstract-Class-and-Pure-Virtual-Function.cpp

---
+++ 11-Abstract-Class-and-Pure-Virtual-Function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual double area() const = 0;
+
+    virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape
+{
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double givenLength, double givenWidth)
+        : length(givenLength), width(givenWidth) {}
+
+    double area() const override
+    {
+        return length * width;
+    }
+};
+
+int main()
+{
+    Rectangle rectangle(7.0, 5.0);
+
+    cout << "Rectangle Area: "
+         << rectangle.area() << '\n';
+
+    return 0;
+}
